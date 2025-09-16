98a2f1998155494348169d553134694fa0788451
cmhfahim <cmhfahim@gmail.com>
2025-09-16 20:58:48 +0600

    Update admin.py

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,1 +1,16 @@
+from django.contrib import admin
+from .models import DynamicTable, DynamicField
+
+@admin.register(DynamicTable)
+class DynamicTableAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+    search_fields = ("name",)
+    ordering = ("-created_at",)
+
+
+@admin.register(DynamicField)
+class DynamicFieldAdmin(admin.ModelAdmin):
+    list_display = ("table", "field_name", "field_type")
+    search_fields = ("field_name",)
+    list_filter = ("field_type",)
 
