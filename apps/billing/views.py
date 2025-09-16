7e91fae5f8fadc834c3623e43cb28925aa2d71a3
cmhfahim <cmhfahim@gmail.com>
2025-09-16 21:40:57 +0600

    Update views.py

--- apps/billing/views.py
+++ apps/billing/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from apps.billing.models import BillingHistory
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def billing_dashboard(request):
+    company = request.user.company
+    bills = BillingHistory.objects.filter(company=company)
+    return render(request, 'billing/billing_dashboard.html', {'bills': bills})
 
