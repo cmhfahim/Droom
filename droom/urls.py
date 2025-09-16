2a8c16cf439df41820f434760da465eb4489c2e7
cmhfahim <cmhfahim@gmail.com>
2025-09-16 20:47:29 +0600

    Update urls.py

--- droom/urls.py
+++ droom/urls.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('apps.core.urls')),
+    path('users/', include('apps.users.urls')),
+    path('billing/', include('apps.billing.urls')),
+    path('api/', include('apps.api.urls')),
+]
 
