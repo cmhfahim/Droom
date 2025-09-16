aac13d09f59f75793b57d1ab5c4564a76b9b9cf4
cmhfahim <cmhfahim@gmail.com>
2025-09-16 21:36:45 +0600

    Update admin.py

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,1 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import CustomUser  # Assuming you created a CustomUser model
+
+# Register your custom user model for admin
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
+
+# If you have other models in users app, register them here
+# Example: CompanyUserRelation
+# from .models import CompanyUserRelation
+# admin.site.register(CompanyUserRelation)
 
