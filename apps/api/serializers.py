a1bdad1e0c3234f72a6cb384e6c13e7c0fbdcdc9
cmhfahim <cmhfahim@gmail.com>
2025-09-16 21:42:38 +0600

    Update serializers.py

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
+from apps.core.models import CompanyData
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyData
+        fields = '__all__'
 
