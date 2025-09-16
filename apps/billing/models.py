fd8a2ff6a9a09dd36726605115832675444dc3da
cmhfahim <cmhfahim@gmail.com>
2025-09-16 21:39:55 +0600

    Update models.py

--- apps/billing/models.py
+++ apps/billing/models.py
@@ -1,1 +1,9 @@
+from django.db import models
+from apps.core.models import CompanyData
+
+class BillingHistory(models.Model):
+    company = models.ForeignKey(CompanyData, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    amount = models.FloatField()
+    method = models.CharField(max_length=50)
 
