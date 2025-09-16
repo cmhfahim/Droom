26f4a7c7a52ece455dcb5cbdb7af35bd639bcd46
cmhfahim <cmhfahim@gmail.com>
2025-09-16 21:42:23 +0600

    Update views.py

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,1 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from apps.api.serializers import CompanySerializer
+from apps.core.models import CompanyData
+
+class CompanyListAPI(APIView):
+    def get(self, request):
+        companies = CompanyData.objects.all()
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
 
